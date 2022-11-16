1d9058c19e138621991ffa4635c02c08f74ca8fa
mldamico <matias.lioneldamico@gmail.com>
2022-11-16 17:03:26 -0300

    Get image by name

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,11 +1,15 @@
 import {
   BadRequestException,
   Controller,
+  Get,
+  Param,
   Post,
+  Res,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { Response } from 'express';
 import { diskStorage } from 'multer';
 import { FilesService } from './files.service';
 import { fileNamer, fileFilter } from './helpers';
@@ -13,6 +17,15 @@ import { fileNamer, fileFilter } from './helpers';
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
+
+  @Get('product/:imageName')
+  findProductImage(
+    @Res() res: Response,
+    @Param('imageName') imageName: string,
+  ) {
+    const path = this.filesService.getStaticProductImage(imageName);
+    res.sendFile(path);
+  }
 
   @Post('product')
   @UseInterceptors(
@@ -29,6 +42,7 @@ export class FilesController {
     if (!file) {
       throw new BadRequestException('Make sure that the file is an image');
     }
-    return file;
+    const secureUrl = `${file.filename}`;
+    return { secureUrl };
   }
 }
