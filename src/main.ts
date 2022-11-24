f208e180eee08f495b8955c309b4fe1891e63a28
mldamico <matias.lioneldamico@gmail.com>
2022-11-24 16:22:30 -0300

    Sockets basic folders and installations

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,8 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   app.setGlobalPrefix('api');
+  // app.enableCors()
+
   await app.listen(process.env.PORT);
 }
 bootstrap();
